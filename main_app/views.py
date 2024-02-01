018e7f04b3c6e70e42db1449494f7fcdd8a6ff99
Fesomibe <francisesomibe1@gmail.com>
2024-01-31 19:06:41 -0600

    part 4

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -17,8 +18,10 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  # instantiate  FeedingForm to be rendered in detail.html
+  feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', { 
-    'finch': finch 
+    'finch': finch , 'feeding_form': feeding_form
   })
 
 class FinchCreate(CreateView):
@@ -32,3 +35,19 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    #because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+  
